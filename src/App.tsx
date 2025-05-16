e6216e2870513ff3608d0c040fe44fab94b6992d
Chris Chudleigh <chrischudleigh@gmail.com>
2025-05-16 16:50:46 +1000

    added react-hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import { Navbar1 } from "./components/navbar1";
 import image from "./assets/image.jpg";
+import RecipeForm from "./components/menuform";
 
 function App() {
-    return (
-        <>
-            <Navbar1 />
-            <div>
-                <img src={image} alt="Food image" />{" "}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Navbar1 />
+      <RecipeForm />
+      <div>
+        <img src={image} alt="Food image" />{" "}
+      </div>
+    </>
+  );
 }
 
 export default App;
