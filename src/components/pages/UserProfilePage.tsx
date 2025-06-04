d35782127cd5d5c93283ce05edb1be82d440a8c2
Chris Chudleigh <chrischudleigh@gmail.com>
2025-06-04 10:13:51 +1000

    add json-decode to get user name from token for profile page

--- src/components/pages/UserProfilePage.tsx
+++ src/components/pages/UserProfilePage.tsx
@@ -1,9 +1,27 @@
 import { useLoaderData, Link } from "react-router";
 import { assertIsRecipesResult } from "../types.tsx";
+import { jwtDecode } from "jwt-decode";
+import { useState, useEffect } from "react";
 
 export function UserProfilePage() {
   const favs = useLoaderData();
   assertIsRecipesResult(favs);
+
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        setUsername(
+          decodedToken.username || decodedToken.name || decodedToken.sub
+        );
+      } catch (error) {
+        console.error("Error decoding token:", error);
+      }
+    }
+  }, []);
   // check favs received
   //   if (!isObject(favs)) {
   //     throw new Error("favs not an object");
@@ -21,7 +39,7 @@ export function UserProfilePage() {
   return (
     <>
       <div>
-        <h2 className="text-left text-xl mt-3">User Profile</h2>
+        <h2 className="text-left text-xl mt-3">User Profile for {username}</h2>
       </div>
       <h2 className="text-left text-xl mt-3">Favourite Recipes</h2>
       {favs.recipes.length === 0 ? (
